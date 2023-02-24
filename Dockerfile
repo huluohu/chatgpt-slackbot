ec4b4c9e681a62b7fb676bd9b20c94eca004c09e
hzguxiaolong@corp.netease.com <hzguxiaolong@corp.netease.com>
2023-02-24 23:31:55 +0800

    feat(配置): 增加超时时间

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM node:18
+WORKDIR /app
+ADD package.json yarn.lock /app/
+#RUN apt update
+#RUN apt install chromium
+#RUN apt install chromium-browser
+#RUN export PUPPETEER_SKIP_DOWNLOAD='true'
+RUN yarn install
+ADD . /app
+EXPOSE 4005 3002
+CMD ["yarn","--","run","start"]
--- app.ts
+++ app.ts
@@ -1,20 +1,28 @@
 import dotenv from 'dotenv-safe'
-import delay from 'delay'
-import { ChatGPTAPI } from 'chatgpt'
+import {ChatGPTUnofficialProxyAPI} from 'chatgpt'
+import debounce from 'debounce-promise';
 
 dotenv.config()
+const openaiTimeout = process.env.OPENAI_TIME_OUT;
 
-const { App } = require('@slack/bolt');
+const {App} = require('@slack/bolt');
 
 // Initializes your app with your bot token and signing secret
 const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
-  socketMode: true, // add this
-  appToken: process.env.SLACK_APP_TOKEN // add this
+    token: process.env.SLACK_BOT_TOKEN,
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+    appToken: process.env.SLACK_APP_TOKEN,
+    socketMode: true,
+    port : 3002
 });
 
-const chatAPI = new ChatGPTAPI({ apiKey: process.env.OPENAI_API_KEY });
+// const chatAPI = new ChatGPTAPI({ apiKey: process.env.OPENAI_API_KEY });
+const chat = new ChatGPTUnofficialProxyAPI({
+    accessToken: process.env.OPENAI_ACCESS_TOKEN!,
+    // apiReverseProxyUrl: process.env.API_REVERSE_PROXY_URL,
+    apiReverseProxyUrl: 'https://gpt.pawan.krd/backend-api/conversation',
+    debug: true
+})
 
 // Save conversation id
 let conversationId: string
@@ -22,76 +30,128 @@ let parentMessageId: string
 
 // --------------------
 
+const updateMessage = debounce(async ({channel, ts, text, payload}: any) => {
+    await app.client.chat.update({
+        channel: channel,
+        ts: ts,
+        text: text,
+        metadata: payload ? {
+            event_type: "chat_gpt",
+            event_payload: payload
+        } : undefined
+    });
+}, 400);
+
 // Listens to incoming messages that contain "hello"
-app.message(async ({ message, say }) => {
-  try {
-    // say() sends a message to the channel where the event was triggered
-    const prompt = message.text.replace(/(?:\s)<@[^, ]*|(?:^)<@[^, ]*/, '')
-    let msg = "<@" + message.user + "> You asked:\n";
-    msg += ">" + message.text;
+app.message(async ({message, say}) => {
+    console.log('on_message:' + JSON.stringify(message) + "\r\n");
+    console.log('===========================================================\r\n');
 
-    console.log("DM: " + msg)
-    res = await chatAPI.sendMessage(prompt)
-    response = res.text
+    const isUserMessage = message.type === "message" && !message.subtype && !message.bot_id;
+    if (isUserMessage && message.text && message.text !== "reset") {
+        const {messages} = await app.client.conversations.history({
+            channel: message.channel,
+            latest: message.ts,
+            inclusive: true,
+            include_all_metadata: true,
+            limit: 2
+        });
 
-    console.log("Response to @" + message.user +":\n" + response)
+        const previous = (messages || [])[1]?.metadata?.event_payload as any || {
+            parentMessageId: undefined,
+            conversationId: undefined
+        };
 
-    await say(response);
-  } catch (err) {
-    await say("ERROR: Something went wrong, please try again after a while.")
-    console.log(err)
-  }
+        const ms = await say({
+            channel: message.channel,
+            text: ':thought_balloon:',
+        });
+
+
+        let answerText: string;
+        try {
+            const answer = await chat.sendMessage(message.text, {
+                parentMessageId: previous.parentMessageId,
+                conversationId: previous.conversationId,
+                timeoutMs : Number(openaiTimeout),
+                onProgress: async (answer) => {
+                    // Real-time update
+                    answerText = answer.text;
+                    await updateMessage({
+                        channel: ms.channel,
+                        ts: ms.ts,
+                        text: answerText,
+                        payload: answer,
+                    });
+                }
+            });
+
+            console.log("Response to @" + message.user + ":\n" + answerText)
+
+            await updateMessage({
+                channel: ms.channel,
+                ts: ms.ts,
+                text: `${answerText} :end:`,
+                payload: answer,
+            });
+        } catch (error) {
+            await say("ERROR: Something went wrong, please try again after a while：" + JSON.stringify(error));
+            console.log(error);
+        }
+    }
+});
+
+app.message("reset", async ({message, say}) => {
+    console.log('reset：' + message.channel + "\r\n");
+    console.log('===========================================================\r\n');
+    conversationId = ""
+    parentMessageId = ""
+    await say({
+        channel: message.channel,
+        text: 'I reset your session',
+    });
 });
 
 
 // Listens to mention
-app.event('app_mention', async ({ event, context, client, say }) => {
-  console.log("Mention: " + event.text)
-  const prompt = event.text.replace(/(?:\s)<@[^, ]*|(?:^)<@[^, ]*/, '')
+app.event('app_mention', async ({event, context, client, say}) => {
+    console.log('on_app_mention:' + JSON.stringify(event) + "\r\n");
+    console.log('===========================================================\r\n');
+    const question = event.text.replace(/(?:\s)<@[^, ]*|(?:^)<@[^, ]*/, '')
 
-  if (prompt.includes("RESET_THREAD")) {
-	  // RESET THREAD
-	chatAPI.resetThread()
+    try {
+        // reply
+        let answerText = "<@" + event.user + "> You asked:\n";
+        answerText += ">" + question + "\n";
+        const answer = await chat.sendMessage(question, {
+            parentMessageId: parentMessageId,
+            conversationId: conversationId,
+            timeoutMs : Number(openaiTimeout)
+        });
 
-	conversationId = ""
-	parentMessageId = ""
-	
-  	let msg = "<@" + event.user + "> asked to Reset the thread. Started a new conversation.";
+        if (answer.conversationId) {
+            conversationId = answer.conversationId;
+        }
 
-  	await say(msg);
+        if (answer.id) {
+            parentMessageId = answer.id;
+        }
 
-  } else {
-	  try {
-		// reply
-		let msg = "<@" + event.user + "> You asked:\n";
-		msg += ">" + prompt + "\n";
-		let response: string
+        answerText += answer.text;
 
-		res = await chatAPI.sendMessage(prompt, {
-			conversationId,
-			parentMessageId
-		})
-		if (res.conversationId) {
-		  conversationId = res.conversationId
-		}
-
-		if (res.messageId) {
-		      parentMessageId = res.messageId
-		}
-
-		msg += res.text
-
-		await say(msg);
-	  } catch (err) {
-		  await say("ERROR: Something went wrong, please try again after a while.")
-		  console.log(err)
-	  }
-  }
+        await say({
+            channel: event.channel,
+            text: answerText,
+        });
+    } catch (error) {
+        await say("ERROR: Something went wrong, please try again after a while：" + JSON.stringify(error));
+        console.log(error)
+    }
 
 });
 
 (async () => {
-  await app.start();
+    await app.start();
 
-  console.log('⚡️ Bolt app is running at port 4000!');
+    console.log('⚡️ Bolt app is running at port 4000!');
 })();
